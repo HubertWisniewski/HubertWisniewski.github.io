3047a70c93db7bd48aaa16daa171be8072382277
Hubert Wiśniewski <hubert_w@autograf.pl>
2020-06-08 20:57:56 +0200

    Looks ready

--- src/App.js
+++ src/App.js
@@ -22,12 +22,39 @@ class App extends Component {
 
   componentDidMount(){
     this.update()
+    this.startLoading()
   }
+
+  startLoading = () => { 
+    let count = 1
+    
+    window.setInterval(() => {
+   
+      let countString = count.toString()
+      if (count <= 8) {
+        document.getElementById('Bar' + countString).style.backgroundColor = 'orangered'
+        count = count + 1
+      }
+      else {
+        document.getElementById('Bar1').style.backgroundColor = 'black'
+        document.getElementById('Bar2').style.backgroundColor = 'black'
+        document.getElementById('Bar3').style.backgroundColor = 'black'
+        document.getElementById('Bar4').style.backgroundColor = 'black'
+        document.getElementById('Bar5').style.backgroundColor = "black"
+        document.getElementById('Bar6').style.backgroundColor = 'black'
+        document.getElementById('Bar7').style.backgroundColor = 'black'
+        document.getElementById('Bar8').style.backgroundColor = 'black'
+
+        count = 1
+      }
+      
+  }, 1000) }
 
   showAbout = () => {
     this.setState({
       sectionsOn:true
     })
+    document.getElementById('ProjectSection').style.opacity = 0
     document.getElementById('ContactSection').style.opacity = 0
     document.getElementById('ContactSection').style.zIndex = -2
     document.getElementById('AboutSection').style.opacity = 1
@@ -41,11 +68,26 @@ class App extends Component {
     this.setState({
       sectionsOn:true
     })
+    document.getElementById('ProjectSection').style.opacity = 0
     document.getElementById('ContactSection').style.zIndex = 3
     document.getElementById('ContactSection').style.opacity = 1
     document.getElementById('AboutSection').style.opacity = 0
     document.getElementById('Desc').style.animation = 'none'
     document.getElementById('DescTwo').style.animation = 'navbarslide 5s ease 0s 1 normal forwards'
+    document.getElementById('App').style.backgroundColor = 'black'
+    document.getElementById('Image').style.animation = 'filters 5s ease 0s 1 normal forwards'
+  }
+
+  showProjects = () => {
+    this.setState({
+      sectionsOn:true
+    })
+    document.getElementById('ProjectSection').style.opacity = 1
+    document.getElementById('ContactSection').style.zIndex = -2
+    document.getElementById('ContactSection').style.opacity = 0
+    document.getElementById('AboutSection').style.opacity = 0
+    document.getElementById('Desc').style.animation = 'none'
+    document.getElementById('DescTwo').style.animation = 'none'
     document.getElementById('App').style.backgroundColor = 'black'
     document.getElementById('Image').style.animation = 'filters 5s ease 0s 1 normal forwards'
   }
@@ -76,7 +118,8 @@ class App extends Component {
 
      const navbutton3 = document.createElement('div', {className: 'klocek'})
      navbutton3.classList.add('klocek')
-     navbutton3.innerHTML = 'Skills'
+     navbutton3.onclick = () => {this.showProjects()}
+     navbutton3.innerHTML = 'Projects'
      document.getElementById('Start').appendChild(navbutton3)
   }
 
@@ -124,7 +167,7 @@ class App extends Component {
     // var left = parseFloat(window.getComputedStyle(baloon).left);
   
     bottom += element.speed;
-        if (bottom >= 570) {
+        if (bottom >= 870) {
           element.remove()
         }
         
@@ -147,12 +190,25 @@ class App extends Component {
     <div id='App' className="wrapper">
   <img id='Image' src={me} alt='me' style={this.state.sectionsOn ? {zIndex: 2} : {zIndex: -1} && this.state.sectionsOn ? {opacity: 1} : {opacity: 0} }/>
   <div id="AboutSection" className="About" style={this.state.sectionsOn ? {zIndex: 3} : {zIndex: -1} && this.state.sectionsOn ? {opacity: 1} : {opacity: 0} }>My name is <br/> <br/>Hubert Wiśniewski <hr className='line'/>
-  <h1 id='Desc' style={{ color: '#dac2a1'}}>I am an aspiring FrontEnd Developer <br/><br/>based in Poland<br/><br/><br/><br/>I like unorthodox designs <br/><br/> <br/><br/><br/>I am also a guitarist/singer in my band <br/><br/><p style={{color: '#f86502'}}>'The Cassino'</p>  </h1>
+  <h1 id='Desc' style={{ color: '#dac2a1'}}>I am an aspiring FrontEnd Developer <br/><br/>based in Poland<br/><br/><br/><br/>I do enjoy unorthodox designs <br/><br/> <br/><br/><br/>I am also a guitarist/singer in my band <br/><br/><p style={{color: '#f86502'}}>'The Cassino'</p>  </h1>
   </div>
   <div id='ContactSection' className='Contact'  style={this.state.sectionsOn ? {zIndex: 1001} : {zIndex: -1} && this.state.sectionsOn ? {opacity: 1} : {opacity: 0} }>You can find me<br/><br/> HERE <hr className='line'/>
   <h1 id='DescTwo' style={{ color: '#dac2a1'}}><br/><br/>Email me at: <br/><br/><p style={{color: '#f86502'}} >hubertwisniewski.frontend@gmail.com</p><br/>You can also check: <br/><br/><a href='https://github.com/HubertWisniewski' target="_blank" rel='noopener noreferrer'><img className="social"  src={github} alt='github'/></a><a href='https://linkedin.com/in/hubertwiśniewski' style={{textDecoration: 'none'}} target="_blank" rel='noopener noreferrer'> <img className='social' src={linkedin} style={{marginLeft: '1em'}} alt=''/></a> </h1>
   </div>
-  <div className='toWebsite' id='Start' onClick={(event) => this.websiteMode(event)} style={this.state.counter >= 10 ? {zIndex: 1} : {zIndex: 1}}>Start</div>
+  <div id='ProjectSection' className='Project' style={this.state.sectionsOn ? {zIndex: 1001} : {zIndex: -1} && this.state.sectionsOn ? {opacity: 1} : {opacity: 0} }>
+    <h1 id='DescThree'>Under construction</h1>
+  <div className='Loading'>
+    <div id="Bar1"></div>
+    <div id="Bar2"></div>
+    <div id="Bar3"></div>
+    <div id="Bar4"></div>
+    <div id="Bar5"></div>
+    <div id="Bar6"></div>
+    <div id="Bar7"></div>
+    <div id="Bar8"></div>
+  </div>
+  </div>
+  <div className='toWebsite' id='Start' onClick={(event) => this.websiteMode(event)} style={this.state.counter >= 10 ? {zIndex: 1} : {zIndex: -1}}>Start</div>
   <div className="counter" style={this.state.websiteMode ? {opacity: 0} : {opacity: 1} && this.state.scoreAppeared ? {zIndex: 1} : {zIndex: -1}}>Score: {this.state.counter}</div>
       <div className="menu">
       <div className="one" style={this.state.websiteMode ? {opacity: 0} : {opacity: 1}}>G00D</div>
